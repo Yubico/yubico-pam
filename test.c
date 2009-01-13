08e7f723ce3b9d9107a31d8207cf027cd70914f3
Simon Josefsson <simon@josefsson.org>
2009-01-13 14:08:39 +0000

    Indent.

--- test.c
+++ test.c
@@ -32,12 +32,13 @@
 
 #include <security/pam_appl.h>
 
-int main(int argc, char **argv)
+int
+main (int argc, char **argv)
 {
   pam_handle_t *pamh = NULL;
   int rc;
 
-  rc = pam_sm_authenticate(pamh, 0, 1, argv);
+  rc = pam_sm_authenticate (pamh, 0, 1, argv);
 
   printf ("rc %d\n", rc);
 
