afd4198de1f2bc658c285021271d3e109716de38
Fredrik Thulin <fredrik@yubico.com>
2011-12-06 14:38:08 +0100

    Fix implicit declaration warning.

--- test.c
+++ test.c
@@ -30,7 +30,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* These #defines must be present according to PAM documentation. */
+#define PAM_SM_AUTH
+
+#ifdef HAVE_SECURITY_PAM_APPL_H
 #include <security/pam_appl.h>
+#endif
+#ifdef HAVE_SECURITY_PAM_MODULES_H
+#include <security/pam_modules.h>
+#endif
+
 
 int
 main (int argc, char **argv)
