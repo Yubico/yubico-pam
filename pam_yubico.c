82296f46320d2bfa8950f85da1124ab3882381eb
Fredrik Thulin <fredrik@yubico.com>
2012-02-10 15:13:42 +0100

    do_challenge_response: Clear errno when done.

--- pam_yubico.c
+++ pam_yubico.c
@@ -628,6 +628,7 @@ do_challenge_response(pam_handle_t *pamh, struct cfg *cfg, const char *username)
 
   DBG(("Challenge-response success!"));
   errstr = NULL;
+  errno = 0;
 
  out:
   if (yk_errno) {
