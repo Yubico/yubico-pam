9cdf8f76cd605b91b2b24e03a36a5b493050a6c0
Klas Lindfors <klas@yubico.com>
2015-01-20 10:53:47 +0100

    update build-and-test

--- tests/aux/build-and-test.sh
+++ tests/aux/build-and-test.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
 set -e
+set -x
 
 autoreconf -i
 
-./configure $COVERAGE
+./configure $CONFIGURE_ARGS $COVERAGE
 make check
 if [ "x$COVERAGE" != "x" ]; then
     gem install coveralls-lcov
+    set +x
     coveralls-lcov --repo-token $COVERALLS_TOKEN coverage/app2.info
 fi
